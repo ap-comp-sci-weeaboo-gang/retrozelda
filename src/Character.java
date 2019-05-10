0ec895534007e5adb1751577f043d57d96f0b54f
Steven Ren <49923377+hr1646@users.noreply.github.com>
2019-05-10 13:56:38 -0700

    Update Character.java

--- src/Character.java
+++ src/Character.java
@@ -11,8 +11,25 @@ public abstract class Character {
     private Image leftImg;
     private Rectangle rect;
     private Robot robot;
+    private int totalhp;
+    private int currenthp=3;
+    private int rupees;
+    private int [] linksitems;
+    private boolean bow = false;
+    private boolean bluecandle = false;
+    private boolean redcandle = false;
+    private boolean boomerang = false;
+    private boolean magicalboomerang = false;
+    private boolean silverarrows = false;
+    private boolean whistle = false;
+    private boolean food = false;
+    private boolean lifePotion= false;
+    private boolean secondlifePotion= false;
+    private boolean magicalRod= false;
+    private boolean bookofmagic= false;
+    private boolean bomb= false;
 
-    public Character (Image up, Image right, Image down, Image left, int x, int y, int w) {
+    public Character (Image up, Image right, Image down, Image left, int x, int y, int w, int hp, int money) {
         upImg = up;
         rightImg = right;
         downImg = down;
